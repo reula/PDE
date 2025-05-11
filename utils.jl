92784545d1b6ee782656bf5fef55dfe2441ef515
Joaquin Pelle <pelle.joaquin@gmail.com>
2025-05-11 18:34:00 +0200

    Add weave utils

--- utils.jl
+++ utils.jl
@@ -1,3 +1,6 @@
+using Franklin
+using Weave
+
 function hfun_bar(vname)
   val = Meta.parse(vname[1])
   return round(sqrt(val), digits=2)
@@ -13,4 +16,37 @@ function lx_baz(com, _)
   brace_content = Franklin.content(com.braces[1]) # input string
   # do whatever you want here
   return uppercase(brace_content)
+end
+
+"""
+    weaveall()
+
+Weave all lecture notes in the `_weave` directory. Run from site root.
+"""
+function weaveall()
+    for (root, _, files) in walkdir("_weave")
+        for file in files
+            if endswith(file, ".ipynb")
+                @info "Weaving Document: $(joinpath(root, file))"
+                weave(joinpath(root, file); out_path=:doc, mod=Main)
+            end
+        end
+    end
+end
+
+
+"""
+    cleanall()
+
+Cleanup all Weave generated subdirectories. Run from site root.
+"""
+function cleanall()
+    for (root, dirs, _) in walkdir("_weave")
+        for dir in dirs
+            if startswith(dir, "jl_")
+                @info "Removing Directory: $(joinpath(root, dir))"
+                rm(joinpath(root, dir); recursive=true, force=true)
+            end
+        end
+    end
 end
